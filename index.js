45a388f15825a6358123b6d1ef91e7622a090b8d
Chris Miller <cmille142@gmail.com>
2014-02-21 10:38:09 -0800

    separate out cli module

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 /*global module, require*/
 
 module.exports = {
-
   //Constructors
-  Benchmark : require('./lib/benchmark'),
+  Benchmark       : require('./lib/benchmark'),
   DefaultReporter : require('./lib/defaultreporter'),
-  Monitor : require('./lib/monitor'),
-  Steps : require('./lib/steps'),
-  StopWatch : require('./lib/stopwatch'),
-  Worker : require('./worker'),
+  Monitor         : require('./lib/monitor'),
+  Steps           : require('./lib/steps'),
+  StopWatch       : require('./lib/stopwatch'),
+  Worker          : require('./lib/worker'),
 
   //Static Methods
-  generator : require('./lib/generator'),
-  logger : require('./lib/logger'),
+  generator       : require('./lib/generator'),
+  logger          : require('./lib/logger'),
 
   workers : {
-    BaseWorker : require('./lib/workers/baseworker'),
-    FayeWorker : require('./lib/workers/fayeworker'),
-    PrimusWorker : require('./lib/workers/primusworker'),
+    BaseWorker     : require('./lib/workers/baseworker'),
+    FayeWorker     : require('./lib/workers/fayeworker'),
+    PrimusWorker   : require('./lib/workers/primusworker'),
     SocketIOWorker : require('./lib/workers/socketioworker')
   }
 };
